49d85473db578af28c0402b3f18ba55336d024ee
usenbekov <altosh.mail@gmail.com>
2021-01-05 14:54:47 +0600

    Initial Commit

---
+++ SyncingTasks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SyncingTasks
+//
+//  Created by Altynbek Usenbekov on 5/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SyncingTasks/SyncingTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncingTasksApp.swift
+//  SyncingTasks
+//
+//  Created by Altynbek Usenbekov on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SyncingTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
